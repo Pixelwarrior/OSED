c9b4d819f632269014d1a6d92e9a7503ca661260
Pixelwarrior <46878481+Pixelwarrior@users.noreply.github.com>
2023-02-16 15:35:01 -0500

    Adding parser

--- Template python scripts/main.py
+++ Template python scripts/main.py
@@ -1,11 +1,16 @@
 import Connection
-def main():
+import argparse
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-ip", "--server", help="IP of the server")
+    parser.add_argument("-p", "--port", help="Port of the server")
+    args = parser.parse_args()
     
     
     fullBuffer = b"A" * 2000
     connectionSettings = [fullBuffer]
-    buf = Connection.Connection("127.0.0.1", 80, connectionSettings, len(connectionSettings))
+    buf = Connection.Connection(args.server, int(args.port), connectionSettings)
     buf.sendBuffer()
 if __name__ == '__main__':
     main()
